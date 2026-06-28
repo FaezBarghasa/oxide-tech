d4ebc0eacf14723c2f71c7bf29967b0b6253d661
FaezBarghasa <faez.barghasa.org@gmail.com>
2026-06-28 10:39:54 +0330

    feat: add IndustrialDashboard component with layout and page integration

---
+++ src/components/IndustrialDashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IndustrialDashboard = () => {
+  return (
+    <div>
+      {/* Content will be added here */}
+    </div>
+  );
+};
+
+export default IndustrialDashboard;
---
+++ src/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import IndustrialDashboard from './components/IndustrialDashboard';
+
+const Page = () => {
+  return (
+    <IndustrialDashboard />
+  );
+};
+
+export default Page;
